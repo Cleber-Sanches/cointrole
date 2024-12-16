214fff8153e6d5a4f41280d85391a07e68cb0930
Cleber Sanches <138811847+Cleber-Sanches@users.noreply.github.com>
2024-12-15 22:36:45 -0300

    ♻️ refactor: melhora o fluxo de validação e nomes de variáveis

--- src/infrastructure/config/connection.ts
+++ src/infrastructure/config/connection.ts
@@ -1,16 +1,19 @@
-import knex from 'knex';
+import 'dotenv/config';
+import config from './../database/knexfile';
+import knex, { Knex } from 'knex';
 import env from './env';
 
-export const db = knex({
-  client: env.DATABASE_CLIENT,
-  connection: {
-    host: env.DATABASE_HOST,
-    port: env.DATABASE_PORT,
-    user: env.DATABASE_USER,
-    password: env.DATABASE_PASSWORD,
-    database: env.DATABASE_NAME,
-  },
-});
+const environment = (env.NODE_ENV || 'development') as keyof typeof config;
+
+if (!config[environment]) {
+  throw new Error(
+    `Configuração para o ambiente '${String(environment)}' não encontrada no knexfile.`
+  );
+}
+
+console.log(`⚙️ Usando o ambiente: ${String(environment)}`);
+
+export const db = knex(config[environment] as Knex.Config);
 
 export const checkDatabaseConnection = async () => {
   try {
