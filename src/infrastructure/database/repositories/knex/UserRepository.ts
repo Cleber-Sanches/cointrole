b88fd39a155286f54bfdb6b2028bd7bb45ca0160
Cleber Sanches <138811847+Cleber-Sanches@users.noreply.github.com>
2024-12-15 22:32:23 -0300

    ✨ feat: adiciona repositório de usuário e metodo de buscar usuario por email

---
+++ src/infrastructure/database/repositories/knex/UserRepository.ts
@@ -0,0 +1,16 @@
+import { User } from '../../../../domain/entities/User';
+import { BaseRepository } from 'bq-knex-base-repository';
+import { db } from '../../../config/connection';
+import { IUserRepositoryAssign } from '../assign/userAssign';
+
+export class KnexUserRepository extends BaseRepository<User> implements IUserRepositoryAssign {
+  constructor() {
+    super(db, 'users');
+  }
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    const result = await db('users').where('email', email).first();
+
+    return result;
+  }
+}
