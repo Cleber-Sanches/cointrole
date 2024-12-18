105f8b09908aedd1cadaf3fcf26f5b1dffef5b9c
Cleber Sanches <138811847+Cleber-Sanches@users.noreply.github.com>
2024-12-17 22:10:09 -0300

    ✨ feat: adiciona rota de registro

--- src/infrastructure/http/routes/auth.ts
+++ src/infrastructure/http/routes/auth.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+<<<<<<< HEAD
 import { RegisterController } from '../../../presentation/controllers/RegisterController';
 import { KnexUserRepository } from '../../database/repositories/knex/UserRepository';
 import { RegisterUseCase } from '../../../application/usecases/auth/RegisterUseCase';
@@ -10,3 +11,10 @@ const registerUseCase = new RegisterUseCase(userRepository);
 const registerController = new RegisterController(registerUseCase);
 
 authRoutes.post('/register', registerController.handle);
+=======
+import { registerController } from '../../../presentation/controllers/RegisterController';
+
+export const authRoutes = Router();
+
+authRoutes.post('/register', registerController);
+>>>>>>> 53f46ed (✨ feat: adiciona rota de registro)
