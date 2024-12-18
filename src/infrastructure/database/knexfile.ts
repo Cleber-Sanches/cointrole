4346c9147ea90816e2ce25bc373b8e8c982dbabf
Cleber Sanches <138811847+Cleber-Sanches@users.noreply.github.com>
2024-12-17 22:10:38 -0300

    ✨ feat: adiciona ambiente de teste

--- src/infrastructure/database/knexfile.ts
+++ src/infrastructure/database/knexfile.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
+import 'dotenv/config';
 import path from 'path';
 import { Knex } from 'knex';
+import env from '../config/env';
 import env from '../config/env';
 
 const development: Knex.Config = {
@@ -32,6 +34,35 @@ const development: Knex.Config = {
   },
 };
 
+const test: Knex.Config = {
+  client: 'pg',
+  pool: {
+    min: 0,
+    max: 10,
+    createTimeoutMillis: 30000,
+    acquireTimeoutMillis: 30000,
+    idleTimeoutMillis: 1000,
+    reapIntervalMillis: 1000,
+    createRetryIntervalMillis: 100,
+    propagateCreateError: true,
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'migrations'),
+    extension: 'ts',
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'seeds'),
+    extension: 'ts',
+  },
+  connection: {
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    database: `${env.DB_NAME}_test`,
+  },
+};
+
 const production: Knex.Config = {
   client: 'pg',
   pool: {
@@ -53,11 +84,13 @@ const production: Knex.Config = {
     extension: 'ts',
   },
   connection: env.DB_URL,
+  connection: env.DB_URL,
 };
 
-const config: { development: Knex.Config; production: Knex.Config } = {
+const config: { development: Knex.Config; production: Knex.Config; test: Knex.Config } = {
   development,
   production,
+  test,
 };
 
 export default config;
